21caeb679c1434cd9726ae1804282f3da554afe7
geo.xiros <geo.xiros@gmailcom>
2019-03-06 15:42:36 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    const dropArea = $('#drop-area');
+
+    dropArea.on('drag dragstart dragend dragover dragenter dragleave drop', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+    })
+    .on('dragover dragenter', function () {
+            dropArea.addClass('highlight');
+        })
+    .on('dragleave dragend drop', function () {
+            dropArea.removeClass('highlight');
+        })
+    .on('drop', function (e) {
+
+            let files = e.originalEvent.dataTransfer.files;
+
+            console.log(files.length);
+
+            for (let i = 0; i < files.length; i++) {
+                console.log(files[i]);
+            }
+
+    });
+});
